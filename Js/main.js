1c486d370de84edb846dcb282f64be377e7bd9fd
Abdelrahman1Rezk <abdelrhmanrizkibrahim@gmail.com>
2022-10-02 07:00:38 +0200

    Performance update

--- Js/main.js
+++ Js/main.js
@@ -1,52 +1,57 @@
+let path = window.location.pathname.split("/").pop();
 //-----------------------
 let dropdownBtn = document.querySelector('.menu-btn');
 let menuContent = document.querySelector('.menu-content');
-dropdownBtn.addEventListener('click',(e)=>{
+dropdownBtn.addEventListener('click', (e) => {
     e.stopPropagation()
-    if(menuContent.style.display===""){
-        menuContent.style.display="block";
+    if (menuContent.style.display === "") {
+        menuContent.style.display = "block";
     } else {
-        menuContent.style.display="";
+        menuContent.style.display = "";
     }
 });
-document.addEventListener("click",(e) =>{
-    if(e.target !== dropdownBtn && e.target !== menuContent){
-        if(menuContent.style.display==="block"){
-            menuContent.style.display="";
+document.addEventListener("click", (e) => {
+    if (e.target !== dropdownBtn && e.target !== menuContent) {
+        if (menuContent.style.display === "block") {
+            menuContent.style.display = "";
         }
     }
 })
 //-----------------------
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    dots:true,
-    nav:false,
-    autoplay:true,
-    autoplaySpeed:500,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:4
+if (path === 'home.html') {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        dots: true,
+        nav: false,
+        autoplay: true,
+        autoplaySpeed: 500,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
         }
-    }
-})
+    })
+}
 //-----------------------
-let landingPage = document.querySelector('.homePage')
-let imgs = [
-    "l1.webp",
-    "l2.webp",
-    "l3.webp",
-    "l4.webp",
-]
-setInterval(() => {
-    let randVar = Math.floor(Math.random() * imgs.length);
-    landingPage.style.backgroundImage = 'url("images/' + imgs[randVar] + '")';
-    
-},8000)
+if (path === 'home.html') {
+    let landingPage = document.querySelector('.homePage')
+    let imgs = [
+        "l1.webp",
+        "l2.webp",
+        "l3.webp",
+        "l4.webp",
+    ]
+    setInterval(() => {
+        let randVar = Math.floor(Math.random() * imgs.length);
+        landingPage.style.backgroundImage = 'url("images/' + imgs[randVar] + '")';
+
+    }, 8000)
+}
 //-----------------------
